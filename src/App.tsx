d212737dac0c5f40bd6bcb35470fb0665ce54e63
Tushar Bisht <tusharbisht843@gamil.com>
2024-03-16 22:39:25 +0530

    Highlight the selected Genre

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} paddingX={5}>
-          <GenreList setSlectedGenre={setSelectedGenre}></GenreList>
+          <GenreList slectedGenre={selectedGenre} setSlectedGenre={setSelectedGenre}></GenreList>
         </GridItem>
       </Show>
       <GridItem area={"main"}>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,9 +3,10 @@ import { Genre, useGenre } from "../hooks/useGenres"
 import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props{
-    setSlectedGenre: (genre : Genre | null) => void;
+    setSlectedGenre: (genre: Genre | null) => void,
+    slectedGenre: Genre | null;
 }
-const GenreList = ( {setSlectedGenre} : Props) => {
+const GenreList = ( {slectedGenre , setSlectedGenre} : Props) => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10 , 11, 12];
     const { data, isLoading, error } = useGenre();
     if (error) return null;
@@ -16,7 +17,7 @@ const GenreList = ( {setSlectedGenre} : Props) => {
               <ListItem key={item.id} paddingY='5px' >
                   <HStack>
                       <Image boxSize={'32px'} borderRadius={8} src={item.image_background}></Image>
-                      <Button onClick={()=> setSlectedGenre(item)} fontSize='large' variant={'link'}>{item.name}</Button>
+                      <Button fontWeight={item.id === slectedGenre?.id ? 'bold' : 'normal'} onClick={()=> setSlectedGenre(item)} fontSize='large' variant={'link'} >{item.name}</Button>
                   </HStack>
               </ListItem>
         )}
