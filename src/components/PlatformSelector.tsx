dc1b3e3446f71b03c4fca17d21132df783ad7b16
Tushar Bisht <tusharbisht843@gamil.com>
2024-04-11 23:14:17 +0530

    Refactoring - remove duplicate interfaces

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,7 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import UsePlatforms from "../hooks/UsePlatforms";
-import { Platform } from "../hooks/useGames";
+import UsePlatforms, { Platform } from "../hooks/UsePlatforms";
 
 interface Props{
   selectedPlatform: Platform | null;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,8 @@
 import {  useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import apiClient , {FetchResponse} from "../service/api-client";
+import { Platform } from "./UsePlatforms";
 
-export interface Platform{
-    id: number,
-    slug :string,
-    name: string,
-}
 export interface Game {
     id: number;
     name: string;
