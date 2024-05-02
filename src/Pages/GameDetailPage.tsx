dc2ed79f187aacca98af6f748d1cb6623c7a191f
Tushar Bisht <tusharbisht843@gamil.com>
2024-05-02 20:55:45 +0530

    Build expandable text

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,18 +1,19 @@
-
 import { useParams } from "react-router-dom";
 import UseGame from "../hooks/UseGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import ExpandableTest from "../components/ExpandableTest";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = UseGame(slug!);
-
   if (isLoading) return <Spinner />;
   if (error || !game) throw error;
+
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableTest>{game.description_raw}</ExpandableTest>
     </>
   );
 };
---
+++ src/components/ExpandableTest.tsx
@@ -0,0 +1,32 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface Props {
+  children: string;
+}
+const ExpandableTest = ({ children }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+  if (!children) return null;
+  const limit = 300;
+  const setDisc = () => {
+    setExpanded(!expanded);
+  };
+  const summary = expanded ? children : children.substring(0, limit) + "...";
+  if (children.length <= 300) return <Text>children</Text>;
+  return (
+    <Text>
+      {summary}
+      <Button
+        onClick={() => setDisc()}
+        size={"xs"}
+              fontWeight={"bold"}
+              ml={1}
+        colorScheme={"yellow"}
+      >
+        {expanded == true ? "Show less" : "Read More"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableTest;
